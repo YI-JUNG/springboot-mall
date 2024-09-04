c5813079f0bc289c6f041ac8ac10f501a226e7ae
ejun <milk.yjwang@gmail.com>
2024-09-04 21:33:06 +0800

    init Sprint Boot Project

---
+++ src/test/java/com/yijung/springbootmall/SpringbootMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yijung.springbootmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
