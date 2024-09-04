ae3963946cd0a78754548560a8b0c133b543d5f8
ejun <milk.yjwang@gmail.com>
2024-09-04 22:30:53 +0800

    商品功能 - 實作查詢商品

---
+++ src/main/java/com/yijung/springbootmall/dao/ProductDao.java
@@ -0,0 +1,10 @@
+package com.yijung.springbootmall.dao;
+
+import com.yijung.springbootmall.model.Product;
+
+public interface ProductDao {
+
+    Product getProductById(Integer productId);
+
+
+}
---
+++ src/main/java/com/yijung/springbootmall/service/ProductService.java
@@ -0,0 +1,7 @@
+package com.yijung.springbootmall.service;
+
+import com.yijung.springbootmall.model.Product;
+
+public interface ProductService {
+    Product getProductById(Integer productId);
+}
